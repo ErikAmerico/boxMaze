77048720e8f30015f3f4ab5bf35cbd5e4a67332b
Erik Olson <erikamerico1991@gmail.com>
2023-08-22 12:04:04 -0400

    Working rendition

--- src/ColorBoxGrid.jsx
+++ src/ColorBoxGrid.jsx
@@ -10,17 +10,64 @@ function ColorBoxGrid({ colors }) {
     generateRandomColors();
   }, []);
 
-  const checkForWin = () => {
-    // Check if any pair of opposite corners are connected by boxes of the same color
-    if (isConnected(0, 24) && colorBoxes[0].color === colorBoxes[24].color ||
-        isConnected(4, 20) && colorBoxes[4].color === colorBoxes[20].color) {
-      alert("You win!");
-    }
+  const areAllBoxesFrozen = () => {
+    return colorBoxes.every(box => box.isFrozen);
   };
 
+  const checkForLoss = () => {
+    if (areAllBoxesFrozen() && !isConnected(0, 24) && !isConnected(4, 20)) {
+        alert("You lost!");
+        } else {
+            console.log("Loss condition not met");
+        }
+    };
+
+
+const checkForWin = () => {
+    console.log("Checking for win...");
+  console.log("All boxes frozen:", areAllBoxesFrozen());
+  console.log("Opposite corners connected:", isConnected(0, 24), isConnected(4, 20));
+    // Check if any pair of opposite corners are connected by boxes of the same color
+    if ((isConnected(0, 24) && colorBoxes[0].color === colorBoxes[24].color) ||
+        (isConnected(4, 20) && colorBoxes[4].color === colorBoxes[20].color)) {
+      alert("You win!");
+    } else if (areAllBoxesFrozen() && !isConnected(0, 24) && !isConnected(4, 20)) {
+        console.log("Lost condition triggered");
+      alert("You lost!");
+    }
+  };
+  
+// const isConnected = (index1, index2) => {
+//     const visited = new Set();
+//     const stack = [index1];
+  
+//     while (stack.length > 0) {
+//       const currentIndex = stack.pop();
+//       visited.add(currentIndex);
+  
+//       if (currentIndex === index2) {
+//         return true;
+//       }
+  
+//       const neighbors = [currentIndex - 5, currentIndex + 5, currentIndex - 1, currentIndex + 1];
+  
+//       for (const neighbor of neighbors) {
+//         if (!visited.has(neighbor) && neighbor >= 0 && neighbor < colorBoxes.length) {
+//           const neighborBox = colorBoxes[neighbor];
+//           if (neighborBox.isFrozen && neighborBox.color === colorBoxes[index1].color) {
+//             stack.push(neighbor);
+//           }
+//         }
+//       }
+//     }
+  
+//     return false;
+//   };
+  
 const isConnected = (index1, index2) => {
     const visited = new Set();
     const stack = [index1];
+    const targetColor = colorBoxes[index1].color;
   
     while (stack.length > 0) {
       const currentIndex = stack.pop();
@@ -33,11 +80,13 @@ const isConnected = (index1, index2) => {
       const neighbors = [currentIndex - 5, currentIndex + 5, currentIndex - 1, currentIndex + 1];
   
       for (const neighbor of neighbors) {
-        if (!visited.has(neighbor) && neighbor >= 0 && neighbor < colorBoxes.length) {
-          const neighborBox = colorBoxes[neighbor];
-          if (neighborBox.isFrozen && neighborBox.color === colorBoxes[index1].color) {
-            stack.push(neighbor);
-          }
+        if (
+          !visited.has(neighbor) &&
+          neighbor >= 0 &&
+          neighbor < colorBoxes.length &&
+          colorBoxes[neighbor].color === targetColor
+        ) {
+          stack.push(neighbor);
         }
       }
     }
@@ -46,7 +95,6 @@ const isConnected = (index1, index2) => {
   };
   
   
-
   const generateRandomColors = () => {
     const randomColors = Array.from({ length: 25 }, () => ({
       color: getRandomColor(),
@@ -70,10 +118,9 @@ const isConnected = (index1, index2) => {
       return updatedBoxes;
     });
 
-     // Update move count
+
   setMoveCount(moveCount + 1);
 
-  // Check for a win
   checkForWin();
   } else {
     alert("You can't freeze a frozen box!");
