29120d0eef55eaf9cfe6a3318d7362dd58fa7af4
Erik Olson <erikamerico1991@gmail.com>
2023-08-22 02:46:00 -0400

    the ability to win by connecting opposite corners with same color blocks works

--- src/ColorBoxGrid.jsx
+++ src/ColorBoxGrid.jsx
@@ -4,10 +4,48 @@ import './ColorBoxGrid.css';
 
 function ColorBoxGrid({ colors }) {
   const [colorBoxes, setColorBoxes] = useState([]);
+  const [moveCount, setMoveCount] = useState(0);
 
   useEffect(() => {
     generateRandomColors();
   }, []);
+
+  const checkForWin = () => {
+    // Check if any pair of opposite corners are connected by boxes of the same color
+    if (isConnected(0, 24) && colorBoxes[0].color === colorBoxes[24].color ||
+        isConnected(4, 20) && colorBoxes[4].color === colorBoxes[20].color) {
+      alert("You win!");
+    }
+  };
+
+const isConnected = (index1, index2) => {
+    const visited = new Set();
+    const stack = [index1];
+  
+    while (stack.length > 0) {
+      const currentIndex = stack.pop();
+      visited.add(currentIndex);
+  
+      if (currentIndex === index2) {
+        return true;
+      }
+  
+      const neighbors = [currentIndex - 5, currentIndex + 5, currentIndex - 1, currentIndex + 1];
+  
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor) && neighbor >= 0 && neighbor < colorBoxes.length) {
+          const neighborBox = colorBoxes[neighbor];
+          if (neighborBox.isFrozen && neighborBox.color === colorBoxes[index1].color) {
+            stack.push(neighbor);
+          }
+        }
+      }
+    }
+  
+    return false;
+  };
+  
+  
 
   const generateRandomColors = () => {
     const randomColors = Array.from({ length: 25 }, () => ({
@@ -20,6 +58,7 @@ function ColorBoxGrid({ colors }) {
   const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
 
   const freezeAndRandomize = (index) => {
+    if (!colorBoxes[index].isFrozen) {
     setColorBoxes((prevBoxes) => {
       const colorToFreeze = prevBoxes[index].color;
       const updatedBoxes = [...prevBoxes];
@@ -30,6 +69,15 @@ function ColorBoxGrid({ colors }) {
   
       return updatedBoxes;
     });
+
+     // Update move count
+  setMoveCount(moveCount + 1);
+
+  // Check for a win
+  checkForWin();
+  } else {
+    alert("You can't freeze a frozen box!");
+  }
 };
 
 const freezeConnected = (index, targetColor, boxes, visited) => {
@@ -71,6 +119,8 @@ const freezeConnected = (index, targetColor, boxes, visited) => {
       return updatedBoxes;
     });
   };
+
+  
 
   return (
     <div className="ColorBoxGrid">
