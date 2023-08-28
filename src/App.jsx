4af205e8821323aa28d90e416a85fc9b1af8b71d
Erik Olson <erikamerico1991@gmail.com>
2023-08-28 16:03:39 -0400

    turned modal into alert for now for gameplay reasons

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,29 @@ import './App.css';
 const colors = ['red', 'green', 'blue', 'teal', 'magenta', 'purple', 'pink', 'yellow', 'orange', 'brown', 'gray', 'white'];
 
 function App() {
-  const [showRulesModal, setShowRulesModal] = useState(true);
+  // const [showRulesModal, setShowRulesModal] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setShowRulesModal(false);
-    }, 10000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setShowRulesModal(false);
+  //   }, 10000);
+  // }, []);
 
-  const handleCloseRulesModal = () => {
-    setShowRulesModal(false);
-  };
-
+  // const handleCloseRulesModal = () => {
+  //   setShowRulesModal(false);
+  // };
+  
+ 
+  const text = "Connect a top corner to its diagonally opposite bottom corner to win. \nYou cannot click on a box that has already been clicked on. \nAdjoining unfrozen boxes will freeze on click. \nMore additions to come!";
+  
+  function rules() {
+    alert(text);
+  }
+  
   return (
     <div>
-      {showRulesModal && <GameRulesModal onClose={handleCloseRulesModal} />}
+      {/* {showRulesModal && <GameRulesModal onClose={handleCloseRulesModal} />} */}
+      <button onClick={rules}>Click here for rules</button>
       <ColorBoxGrid colors={colors}/>
     </div>
   );
