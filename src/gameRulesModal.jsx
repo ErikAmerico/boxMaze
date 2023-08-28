c0b4946e4a6dae132fe0e8cc1d0e6faa7ab05bcc
Erik Olson <erikamerico1991@gmail.com>
2023-08-28 13:03:34 -0400

    trying to continously build netlify

--- src/gameRulesModal.jsx
+++ src/gameRulesModal.jsx
@@ -17,5 +17,8 @@ function GameRulesModal({ handleCloseRulesModal }) {
   );
 }
 
-
 export default GameRulesModal;
+
+//configured sit configuration build and deploy/continuous deployment
+//changed build command from npm run build to CI= npm run build
+//added environment variable CI to value false in netlify
