8ac93892914b8fb5a8a53de5faf7b884c183592c
Erik Olson <erikamerico1991@gmail.com>
2023-08-28 12:36:43 -0400

    added rules modal. pushed to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ColorBoxGrid from './ColorBoxGrid';
+import GameRulesModal from './gameRulesModal';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const colors = ['red', 'green', 'blue', 'teal', 'magenta', 'purple', 'pink', 'yellow', 'orange', 'brown', 'gray', 'white'];
 
 function App() {
+  const [showRulesModal, setShowRulesModal] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowRulesModal(false);
+    }, 10000);
+  }, []);
+
+  const handleCloseRulesModal = () => {
+    setShowRulesModal(false);
+  };
+
   return (
     <div>
+      {showRulesModal && <GameRulesModal onClose={handleCloseRulesModal} />}
       <ColorBoxGrid colors={colors}/>
     </div>
   );
---
+++ src/gameRulesModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './GameRules.css';
+
+function GameRulesModal({ handleCloseRulesModal }) {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Game Rules</h2>
+        <p>Connect a top corner to its diagonally opposite bottom corner to win. <br/>
+           You cannot click on a box that has already been clicked on. <br/>
+           Adjoining unfrozen boxes will freeze on click. <br/>
+           More additions to come! <br/>
+        </p>
+        {/* <button onClick={handleCloseRulesModal}>Close</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default GameRulesModal;
